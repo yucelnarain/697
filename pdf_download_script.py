4dc6718ee7d52981245a12a130d516bafd908d32
Narain Yucel <yucelnarain@gmail.com>
2022-07-05 15:04:00 -0400

    adjusted script v2

--- pdf_download_script.py
+++ pdf_download_script.py
@@ -14,17 +14,17 @@ import urllib.request
 
 
 # %%
-def download_pdfs(sample_number ,original_excel_file, path_for_converted_excel, converted_excel_sheet_name, path_for_pdfs):
+def download_pdfs(sample_number ,original_excel_file, path_to_store_converted_excel, converted_excel_sheet_name, path_for_pdfs):
     """Specify the number of pdfs to download, excel file containing the url links, path for the new converted excel, 
     the sheet name you want to extract files from, and the path for where you want to store the downloaded pdfs which will be scraped from the sheet"""
     #Example command line execution: 
     # python pdf_download_script.py 
     # sample_number:5 
     # original_excel_file: 'DTMB_PRO-Contract-List.xls' 
-    # path_for_converted_excel: '/Users/narainyucel/Google Drive/MADS/capstone/legal_nlp' 
+    # path_to_store_converted_excel: '/Users/narainyucel/Google Drive/MADS/capstone/legal_nlp' 
     # converted_excel_sheet_name: 'Updated 06-16-2022' 
     # path_for_pdfs: '/Users/narainyucel/Google Drive/MADS/capstone/legal_nlp'
-    
+
     #convert old xls to xlsx and split them into one xlsx per sheet, choose sheet with relevant information
     with xw.App(visible=False) as app:
         wb = app.books.open(original_excel_file)
@@ -32,12 +32,12 @@ def download_pdfs(sample_number ,original_excel_file, path_for_converted_excel, 
             wb_new = app.books.add()
             sheet.copy(after=wb_new.sheets[0])
             wb_new.sheets[0].delete()
-            if not os.path.isfile(path_for_converted_excel + f'/{sheet.name}.xlsx'):
-                wb_new.save(path_for_converted_excel + f'/{sheet.name}.xlsx')
+            if not os.path.isfile(path_to_store_converted_excel + f'/{sheet.name}.xlsx'):
+                wb_new.save(path_to_store_converted_excel + f'/{sheet.name}.xlsx')
             wb_new.close()
 
     #Specify xlsx_name, based on the relevant sheet
-    xlsx_file = open(path_for_converted_excel + f'/{converted_excel_sheet_name}.xlsx', 'rb') 
+    xlsx_file = open(path_to_store_converted_excel + f'/{converted_excel_sheet_name}.xlsx', 'rb') 
     out_file = io.StringIO()
     xlsx2html(xlsx_file, out_file, locale='en')
     out_file.seek(0)
